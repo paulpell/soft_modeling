bf9db05c271557779817006ceafc9c5f30f89ddc
paulpell <mail.paulpellegrini@gmail.com>
2012-04-24 16:11:37 +0200

    cloth can fall either on an (invisible) nail, or on the traditional box; still not visually good

--- Code/ClothOnBox.cpp
+++ Code/ClothOnBox.cpp
@@ -1,19 +1,50 @@
 #include "ClothOnBox.h"
-#include "Vector3D.h"
-#include "MassPoint3D.h"
+
+
+// in the old way, a force was exerted by the box on the nearby massPoints
+#define COLLIDE_OLD_WAY 0
+
 
 ClothOnBox::ClothOnBox(){};
 
 ClothOnBox::ClothOnBox (float x, float y, float z) {
-        int segments = 10;
+        int segments = 20;
         float size = 5;
-		//box = new Cube(x,y,z,size);
         box = NULL;
-        nail = new Vertex3D(x + size/2, y + size, z+ size/2);
-		cloth = new Cloth(new MassPoint3D(x - size/2, y + size + 1, z-size/2), segments, 2*size);
-		cloth->applyGlobalForce(new Force(0,-.5,0)); // gravity shall be
-        /*
-        // a force will potentially be exerted on every point 
+		cloth = new Cloth(new MassPoint3D(x - size/2, y + size + 3, z-size/2), segments, 2*size);
+
+#define pt(i,j) (cloth->mesh(i,j))
+#if CLOTHONBOX_NAIL == 1
+        int nail_i=size/2, nail_j = size / 2;
+        MassPoint3D *a = pt(nail_i, nail_j),
+                    *b = pt(nail_i, nail_j+1),
+                    *c = pt(nail_i+1, nail_j),
+                    *d = pt(nail_i+1, nail_j+1);
+        float nx = (a->x + d->x) / 2.;
+        float ny = (a->y + d->y) / 2.;
+        float nz = (a->z + d->z) / 2.;
+
+        // setup a nail
+        nail = new MassPoint3D(nx, ny, nz, 1);
+        nail->setAnchor(true);
+
+        float hardness = 10;
+        nailSprings[0] = new Spring(nail, a, hardness);
+        nailSprings[1] = new Spring(nail, b, hardness);
+        nailSprings[2] = new Spring(nail, c, hardness);
+        nailSprings[3] = new Spring(nail, d, hardness);
+        cloth->bendCorner(nail_i, nail_j, nail);
+#else
+        basex = x;
+        basey = y;
+        basez = z;
+        this->size = size;
+		box = new Cube(x,y,z,size);
+        pushObject(box);
+
+
+#if COLLIDE_OLD_WAY == 1
+        // a force will potentially be exerted on every point by the cube
         forces = new Force**[segments+1];
         for (int i=0; i<segments+1; ++i) {
             forces[i] = new Force*[segments+1];
@@ -23,86 +54,264 @@ ClothOnBox::ClothOnBox (float x, float y, float z) {
                 forces[i][j] = f;
             }
         }
-        pushObject(box);
-        */
+#endif // COLLIDE_OLD_WAY
+
+#endif // CLOTHONBOX_NAIL
+		cloth->applyGlobalForce(new Force(0,-.5,0)); // gravity shall be
         pushObject(cloth);
 }
+
+
+/*** Time step function, animates the cloth ****************/
 
 void ClothOnBox::timeStep(float dt) {
     // box is not moving
     int segments = cloth->getSegments();
-    MassPoint3D *m;
     // a pair of indices is at the same pos in both lists
     list<int> colliding_x, colliding_y;
+
+    // first compute the force
+#if CLOTHONBOX_NAIL == 1
+    for (int i=0; i<4; i++) {
+        nailSprings[i]->timeStep(dt);
+    }
+#endif
 
     cloth->timeStep(dt);
 
 
 
-    /*
+#if CLOTHONBOX_NAIL == 1
+
+#else // the cloth interacts with the box
+    
+#if COLLIDE_OLD_WAY == 1
+    // in this simulation, the box exerts a force on every nearby mass point
+
+    MassPoint3D *m;
     // now check for collision detection
     for (int i=0; i<=segments; i++) {
         for (int j=0; j<= segments; j++) {
             m = cloth->mesh(i,j);
             if (box->isPointInside(m)) {
+                // is p is in the box, we let the box handle
                 box->collide(m, forces[i][j], dt);
-                m->setAnchor(true);
+                //m->setAnchor(true);
+                // save info of which masspoints did collide
                 colliding_x.push_front(i);
                 colliding_y.push_front(j);
             }
-            else {
+            else if (!box->isPointInActiveArea(m)) {
                 forces[i][j]->reset();
+            }
+            else {
+                forces[i][j]->scale(.5);
             }
         }
     }
 
+    
     // one more loop, to avoid cloth inside cube
     list<int>::iterator
         itx  = colliding_x.begin(),
         endx = colliding_x.end(),
         ity  = colliding_y.begin(),
         endy = colliding_y.end();
-
     for (; itx != endx; itx++, ity++) {
+ 
+       //if (box->isPointInBaseSquare(cloth->mesh(*itx, *ity))) {
+             if (*itx > 0) {
+                 if (!box->isPointInBaseSquare(cloth->mesh(*itx-1, *ity))) {
+                     cloth->mesh(*itx-1, *ity)->y = box->maxY();
+                 }
+             }
+             if (*itx < segments) {
+                 if (!box->isPointInBaseSquare(cloth->mesh(*itx+1, *ity))) {
+                     cloth->mesh(*itx+1, *ity)->y = box->maxY();
+                 }
+             }
+             if (*ity > 0) {
+                 if (!box->isPointInBaseSquare(cloth->mesh(*itx, *ity-1))) {
+                     cloth->mesh(*itx, *ity-1)->y = box->maxY();
+                 }
+             }
+             if (*ity < segments) {
+                 if (!box->isPointInBaseSquare(cloth->mesh(*itx, *ity+1))) {
+                     cloth->mesh(*itx, *ity+1)->y = box->maxY();
+                 }
+             }
+       //}
+     }
 
-        if (box->isPointInBaseSquare(cloth->mesh(*itx, *ity))) {
-            if (*itx > 0) {
-                if (!box->isPointInBaseSquare(cloth->mesh(*itx-1, *ity))) {
-                    cloth->mesh(*itx-1, *ity)->y = box->maxY();
+
+#else // COLLIDE_OLD_WAY
+
+
+    // let ths points collide if they enter the box
+    for (int i=0; i<segments+1; i++) {
+        for (int j=0; j<segments+1; j++) {
+            if (box->isPointInside(pt(i,j))) {
+                box->collide(pt(i,j), NULL, dt);
+            }
+        }
+    }
+
+    MassPoint3D *m1, *m2;
+    float mass = 4;
+    
+    for (int i=0; i<segments; i++) {
+        for (int j=0; j<segments; j++) {
+
+            cloth->clearBend(i,j);
+
+
+            MassPoint3D *a = pt(i,j), *b = pt(i, j+1),
+                      *c = pt(i+1, j), *d = pt(i+1, j+1);
+
+            int pos1 = box->relpos(a),
+                pos2 = box->relpos(d);
+
+            // depending on the relative position of both points wrt the cube, 
+            // we may have to insert one or two bending points to the cloth tile.
+            // The logic for that follows, it's not very readable but well....
+            // See Cube.c for the relpos encoding.
+
+            // one point is outside with lower x
+            if ((pos1 & 1) ^ (pos2 & 1)) {
+                // one p with lower y
+                if ((pos1 & 4) ^ (pos2 & 4)) {
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex, basey, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex, basey, basez + size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)) { // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex, basey, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex, basey, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
+                }
+                // one p with higher y
+                else if ((pos1 & 8) ^ (pos2 & 8)) {
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex, basey + size, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex, basey + size, basez +size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)){ // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex, basey + size, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex, basey + size, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
+                }
+                else if (!(pos1 & 4 || pos1 & 8 || pos2 & 4 || pos2 & 8)){ // both inside the box wrt y
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex, basey + size, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex, basey + size, basez +size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)){ // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex, basey + size, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex, basey + size, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
                 }
             }
-            if (*itx < segments) {
-                if (!box->isPointInBaseSquare(cloth->mesh(*itx+1, *ity))) {
-                    cloth->mesh(*itx+1, *ity)->y = box->maxY();
+            else if ((pos1 & 2) ^ (pos2 & 2)) { // one p with higher x
+                // one p with lower y
+                if ((pos1 & 4) ^ (pos2 & 4)) {
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex + size, basey, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex + size, basey, basez + size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)){ // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex + size, basey, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex + size, basey, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
+                }
+                // one p with higher y
+                else if ((pos1 & 8) ^ (pos2 & 8)) {
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex + size, basey + size, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex + size, basey + size, basez +size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)){ // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex + size, basey + size, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex + size, basey + size, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
+                }
+                else if (!(pos1 & 4 || pos1 & 8 || pos2 & 4 || pos2 & 8)){ // both inside the box wrt y
+                    if ((pos1 & 16) ^ (pos2 & 16)) {
+                        m1 = new MassPoint3D(basex + size, basey + size, basez, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if ((pos1 & 32) ^ (pos2 & 32)) {
+                        m1 = new MassPoint3D(basex + size, basey + size, basez +size, mass);
+                        cloth->bendCorner(i,j,m1);
+                    }
+                    else if (!(pos1 & 16 || pos1 & 32 || pos2 & 16 || pos2 & 32)) { // both inside the box wrt to z
+                        m1 = new MassPoint3D(basex + size, basey + size, (a->z + b->z) /2, mass);
+                        m2 = new MassPoint3D(basex + size, basey + size, (c->z + d->z) / 2, mass);
+                        cloth->bendEdge(i, j, m1, m2);
+                    }
                 }
             }
-            if (*ity > 0) {
-                if (!box->isPointInBaseSquare(cloth->mesh(*itx, *ity-1))) {
-                    cloth->mesh(*itx, *ity-1)->y = box->maxY();
+
+            else if(!(pos1 & 1 || pos1 & 2 || pos2 & 1 || pos2 & 2)) { // both inside the box wrt x
+                if (((pos1 & 4) ^ (pos2 & 4)) && ((pos1 & 16) ^ (pos2 & 16))) {
+                    m1 = new MassPoint3D((a->x + b->x) / 2., basey, basez, mass);
+                    m2 = new MassPoint3D((c->x + d->x) / 2., basey, basez, mass);
+                    cloth->bendEdge(i, j, m1, m2);
                 }
-            }
-            if (*ity < segments) {
-                if (!box->isPointInBaseSquare(cloth->mesh(*itx, *ity+1))) {
-                    cloth->mesh(*itx, *ity+1)->y = box->maxY();
+                else if (((pos1 & 4) ^ (pos2 & 4)) && ((pos1 & 32) ^ (pos2 & 32))) {
+                    m1 = new MassPoint3D((a->x + b->x) / 2., basey, basez + size, mass);
+                    m2 = new MassPoint3D((c->x + d->x) / 2., basey, basez + size, mass);
+                    cloth->bendEdge(i, j, m1, m2);
+                }
+                else if (((pos1 & 8) ^ (pos2 & 8)) && ((pos1 & 16) ^ (pos2 & 16))) {
+                    m1 = new MassPoint3D((a->x + b->x) / 2., basey + size, basez, mass);
+                    m2 = new MassPoint3D((c->x + d->x) / 2., basey + size, basez, mass);
+                    cloth->bendEdge(i, j, m1, m2);
+                }
+                else if (((pos1 & 8) ^ (pos2 & 8)) && ((pos1 & 32) ^ (pos2 & 32))) {
+                    m1 = new MassPoint3D((a->x + b->x) / 2., basey + size, basez + size, mass);
+                    m2 = new MassPoint3D((c->x + d->x) / 2., basey + size, basez + size, mass);
+                    cloth->bendEdge(i, j, m1, m2);
                 }
             }
         }
     }
-    */
+
+#endif // COLLIDE_OLD_WAY
+
+#endif // CLOTHONBOX_NAIL
 }
 
 void ClothOnBox::draw() {
-	if (box)
-        box->draw();
-    else {
-        nail->draw();
-/*      
-        glBegin(GL_POINTS);
-        glPointSize(50);
-        glColor3f(.9, .1, .2);
-        glVertex3f(nail->x, nail->y, nail->z);
-        glEnd();
-*/
-    }
+#if CLOTHONBOX_NAIL == 1
+    nail->draw();
+#else
+    box->draw();
+#endif
+
     cloth->draw();
 }
--- Code/ClothOnBox.h
+++ Code/ClothOnBox.h
@@ -5,14 +5,26 @@
 #include "Cube.h"
 #include "Vertex3D.h"
 
+// for fun, let's drop a cloth on a nail
+// cloth bending is required for the nail simulation
+#if CLOTH_BENDING
+#define CLOTHONBOX_NAIL 0
+#endif
+
 class ClothOnBox: public WorldObject {
 
 	Cloth *cloth;
 	Cube *box; // if we don't apply any force, it won't move
     Force ***forces; // force from the box on the cloth
 
-    // for fun, let's drop a cloth on a nail
-    Vertex3D *nail;
+#if CLOTH_BENDING == 1
+    float basex, basey, basez, size;
+#endif
+#if CLOTHONBOX_NAIL == 1
+    MassPoint3D *nail;
+    int nail_i, nail_j; // pos of the nail in the mesh
+    Spring* nailSprings[4];// on its 4 neighbours
+#endif
 
 public:
 	
